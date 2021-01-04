c20a17e4e4c458c7b7ee7f90dcaa325160356d59
Jarkko Bamberg <jarkko.bamberg@gmail.com>
2021-01-05 01:43:10 +0200

    from js to ts

--- dist/utils/config.js
+++ dist/utils/config.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NAMES_URL = exports.PORT = void 0;
 require('dotenv').config();
-exports.PORT = 3765;
+exports.PORT = process.env.PORT || 3765;
 exports.NAMES_URL = 'https://raw.githubusercontent.com/solita/dev-academy-2021/main/names.json';
 //# sourceMappingURL=config.js.map
