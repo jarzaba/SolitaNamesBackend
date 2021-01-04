de13199621e5bc0b1bd20abc1ea2a48646a5d887
Jarkko Bamberg <jarkko.bamberg@gmail.com>
2021-01-04 21:28:13 +0200

    scripts for production build

--- app.js
+++ app.js
@@ -9,14 +9,8 @@ const middleware = require('./utils/middleware');
 app.use(cors());
 app.use(express.json());
 morgan.token('req_body', (req, res) => JSON.stringify(req.body));
-app.use(morgan('tiny', { skip: (req, res) => req.method === 'POST' }));
-app.use(
-  morgan(
-    ':method :url :status :res[content-length] - :response-time ms :req_body',
-    { skip: (req, res) => req.method !== 'POST' }
-  )
-);
-
+app.use(morgan('tiny'));
+app.use(express.static('build'));
 app.use(middleware.requestLogger);
 app.use('/api/names', nameRouter);
 app.use(middleware.unknownEndpoint);
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 let PORT = process.env.PORT || 3001;
-let NAMES_URL = process.env.NAMES_URL;
+let NAMES_URL =
+  'https://raw.githubusercontent.com/solita/dev-academy-2021/main/names.json';
 
 module.exports = {
   NAMES_URL,
