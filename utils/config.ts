37f42c56481b012f98c0f5a3441c05964b16025b
Jarkko Bamberg <jarkko.bamberg@gmail.com>
2021-01-05 01:37:12 +0200

    from js to ts

--- utils/config.ts
+++ utils/config.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
-export const PORT = 3765;
+export const PORT = process.env.PORT || 3765;
 export const NAMES_URL =
   'https://raw.githubusercontent.com/solita/dev-academy-2021/main/names.json';
