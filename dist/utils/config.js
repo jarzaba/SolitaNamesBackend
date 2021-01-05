189ee56d1ccd1415a5d6e16df643aae23e5b5867
Jarkko Bamberg <jarkko.bamberg@gmail.com>
2021-01-05 11:41:01 +0200

    url change

--- dist/utils/config.js
+++ dist/utils/config.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.NAMES_URL = exports.PORT = void 0;
 require('dotenv').config();
 exports.PORT = process.env.PORT || 3765;
-exports.NAMES_URL = process.env.PORT;
+exports.NAMES_URL = 'https://raw.githubusercontent.com/solita/dev-academy-2021/main/names.json';
 //# sourceMappingURL=config.js.map
